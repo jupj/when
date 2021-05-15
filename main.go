cae5933d7b7e7c9ffc4b4f231b80889092c13241
Johan Jansson <johan.jansson@iki.fi>
2021-05-15 08:32:51 +0300

    Add -d option, update usage string

--- main.go
+++ main.go
@@ -9,8 +9,26 @@ import (
 	"path"
 	"strings"
 	"text/tabwriter"
+	"text/template"
 	"time"
 )
+
+var usageTmpl = template.Must(template.New("usage").Parse(`
+{{- . }} - time zone converter
+
+Usage:
+
+  {{.}} [option] zone [zone ...]
+
+Examples:
+
+  {{.}} Europe/Paris America/New_York  # Use IANA Time Zones
+  {{.}} paris new_york                 # Use partial names
+  {{.}} -d 2021-11-07 paris new_york   # Show specified date
+
+Options:
+
+`))
 
 //go:generate go run gen_zones.go -o zones.txt
 
@@ -33,19 +51,51 @@ func find(tzNames []string, name string) []string {
 	return res
 }
 
+// parseDate parses a date in the following formats: dd | mm-dd | yyyy-mm-dd
+func parseDate(str string) (time.Time, error) {
+	now := time.Now()
+
+	if t, err := time.Parse("2", str); err == nil {
+		str = fmt.Sprintf("%.4d-%.2d-%.2d", now.Year(), now.Month(), t.Day())
+	} else if t, err := time.Parse("1-2", str); err == nil {
+		str = fmt.Sprintf("%4.d-%.2d-%.2d", now.Year(), t.Month(), t.Day())
+	}
+
+	// Add current hour to string
+	str = fmt.Sprintf("%s %.2d", str, now.Hour())
+
+	return time.Parse("2006-01-02 15", str)
+}
+
+// run executes the program with the given arguments
 func run(args []string) error {
 	// Set up command flags
+	var date string
 	flags := flag.NewFlagSet("when", flag.ExitOnError)
+	flags.StringVar(&date, "d", date, "show zones at `date` (dd, mm-dd or yyyy-mm-dd)")
+
 	flags.Usage = func() {
-		fmt.Fprintln(flags.Output(), "Usage:")
-		fmt.Fprintln(flags.Output())
-		fmt.Fprintf(flags.Output(), "%s ZONE [ZONE ...]\n", flags.Name())
+		usageTmpl.Execute(flags.Output(), flags.Name())
 		flags.PrintDefaults()
 	}
 	if err := flags.Parse(args); err != nil {
 		return err
 	}
 
+	// Set local time
+	localtime := time.Now().Local()
+	if date != "" {
+		t, err := parseDate(date)
+		if err != nil {
+			return fmt.Errorf("cannot parse -d flag: %w", err)
+		}
+
+		localtime = t.Local()
+	}
+	_, localoffset := localtime.Zone()
+	y, m, d, h := localtime.Year(), localtime.Month(), localtime.Day(), localtime.Hour()
+
+	//
 	names := strings.Split(zones, "\n")
 
 	zones := []*time.Location{time.Local}
@@ -69,11 +119,7 @@ func run(args []string) error {
 	w := tabwriter.NewWriter(os.Stdout, 2, 0, 1, ' ', 0)
 	defer w.Flush()
 
-	now := time.Now().Local()
-	_, localoffset := now.Zone()
-	y, m, d, h := now.Year(), now.Month(), now.Day(), now.Hour()
-
-	fmt.Println(now.Format("Monday 2006-01-02 (MST -07:00)"))
+	fmt.Println(localtime.Format("Monday 2006-01-02 (MST -07:00)"))
 	fmt.Fprintln(w, "Zone\t \u0394t\tTime")
 	for _, z := range zones {
 		// display only Location from Area/Location
